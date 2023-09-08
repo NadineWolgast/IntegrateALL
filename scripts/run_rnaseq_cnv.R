72afa6e577013e2eebf1811dad3b5d51e7818848
NadineKraft <NadineKraft@gmx.de>
2023-09-08 12:11:01 +0200

    Edited config for different samples, added TODOs

--- scripts/run_rnaseq_cnv.R
+++ scripts/run_rnaseq_cnv.R
@@ -8,6 +8,7 @@ args <- commandArgs(trailingOnly = TRUE)
 config_file <- args[1]
 metadata_file <- args[2]
 
-RNAseqCNV_wrapper(config = config_file, metadata = metadata_file, snv_format = "custom", CNV_matrix = T, arm_lvl = T, batch = T, generate_weights = T)
+RNAseqCNV_wrapper(config = config_file, metadata = metadata_file, snv_format = "custom", CNV_matrix = T, arm_lvl = T, batch = F, generate_weights = F)
 
 # TODO: How to save in another directory?
+# TODO: Test with batch = T for more than 20 samples and set generate_weights = T

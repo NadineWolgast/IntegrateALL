8eef8acf20817b400dea63a6fc29ff47323f58bc
NadineKraft <NadineKraft@gmx.de>
2023-10-13 10:10:03 +0200

    Edited rule to calculate tpms. Clean up script.

--- scripts/calculate_tpm.py
+++ scripts/calculate_tpm.py
@@ -1,14 +1,20 @@
 import pandas as pd
+import sys
+
+
+reads_per_gene_file = sys.argv[1]
+total_mapped_reads_file = sys.argv[2]
+outfile = sys.argv[3]
 
 # Load the ReadsPerGene.out.tab file
-df = pd.read_csv(snakemake.input.reads_per_gene, sep='\t', skiprows=snakemake.params.skip_rows, names=["Gene", "Count"])
+df = pd.read_csv(reads_per_gene_file, sep='\t', skiprows=4, names=["Gene", "Count"], index_col=False)
 
 # Load the total mapped reads
-with open(snakemake.input.total_mapped_reads, 'r') as total_reads_file:
+with open(total_mapped_reads_file, 'r') as total_reads_file:
     total_reads = int(total_reads_file.read().strip())
 
 # Calculate TPM
 df["TPM"] = (df["Count"] / df["Count"].sum()) * 1e6 / (total_reads / 1e6)
 
 # Write TPM values to the output file
-df.to_csv(snakemake.output.tpm, sep='\t', index=False)
+df.to_csv(outfile, sep='\t', index=False)
---
+++ scripts/define_subtype_and_caryotype.R
@@ -0,0 +1,95 @@
+library("readr")
+library("utils")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+print(args)
+
+prediction_file <- args[1]
+rna_seq_cnv_estimation_file <- args[2]
+fusioncatcher_file <- args[3]
+arriba_file <- args[4]
+chromosome_counts_karyotype_file <- args[5]
+anno_gene_fusions_file <- args[6]
+outfile <- args[7]
+print(arriba_file)
+
+subtype_data <- read.table(anno_gene_fusions_file, header = TRUE, sep = "")
+
+
+prediction_data <- read.table(prediction_file, header=TRUE, sep="\t")
+
+# Extract sample-specific information
+sample_info <- prediction_data$sample
+subtype <- prediction_data$Prediction
+confidence <- prediction_data$Confidence
+karyotype_data <- read.table(rna_seq_cnv_estimation_file, header=TRUE, sep="\t")
+chromosome_number <- karyotype_data$chrom_n
+
+# Define the dataframe
+chromosome_counts_to_karyotype <- data.frame(
+  Chromosome_count = c("24-30", "31-39", "51-58", "59-78"),
+  Karyotype = c("near haploid", "low hypodiploid", "high hyperdiploid", "near triploid"),
+  Subtype = c("Near haploid", "Low hypodiploid", "Hyperdiploid", "Low hypodiploid")
+)
+
+# Define the function to check Subtype and Chromosome_number
+check_subtype_and_chromosome <- function(Subtype, Chromosome_number,arriba_file, fusioncatcher_file) {
+  # Check if Subtype is in the dataframe
+  if (Subtype %in% chromosome_counts_to_karyotype$Subtype) {
+    # Find the corresponding row for the Subtype
+    matching_row <- chromosome_counts_to_karyotype[chromosome_counts_to_karyotype$Subtype == Subtype, ]
+
+    # Extract the Chromosome_count range for the Subtype
+    chromosome_range <- matching_row$Chromosome_count
+
+    # Check if Chromosome_number falls within the range
+    if (Chromosome_number %in% unlist(strsplit(chromosome_range, "-"))) {
+      return(paste("Bei", Subtype, ": Analysis of the virtual karyotype established a chromosome count of",
+                    Chromosome_number, "confirming the subtype allocation."))
+    } else {
+      return(paste("Bei", Subtype, ": Analysis of the virtual karyotype established a chromosome count of",
+                    Chromosome_number, "not confirming the subtype allocation."))
+    }
+  } else {
+         if (Subtype %in% subtype_data$Subtype) {
+        # Find the matching row in the subtype data
+        matching_row <- subset(subtype_data, Subtype == Subtype)
+        X5_end_partner <- matching_row$X5_end_partner
+        X3_end_partner <- matching_row$X3_end_partner
+
+        # Check if X5_end_partner and X3_end_partner are present in both arriba and fusioncatcher files
+        arriba_data <- read.table(arriba_file, header = TRUE, sep = "\t")
+        fusioncatcher_data <- read.table(fusioncatcher_file, header = TRUE, sep = "\t")
+
+        if (all(X5_end_partner %in% arriba_data$gene1) && all(X3_end_partner %in% arriba_data$gene2) &&
+            all(X5_end_partner %in% fusioncatcher_data$Gene_1_symbol) && all(X3_end_partner %in% fusioncatcher_data$Gene_2_symbol)) {
+          return(paste("Gene fusion calling identified a ", paste(X5_end_partner, X3_end_partner, collapse = " fusion "),
+                       "confirming the subtype allocation."))
+        } else {
+          return(paste("Gene fusion calling identified fusions not confirming the subtype allocation."))
+        }
+      } else {
+        return("Subtype not found in the subtype data.")
+      }
+    }
+}
+
+# Example usage:
+#Subtype <- "Low hypodiploid"
+#Chromosome_number <- 35
+result <- check_subtype_and_chromosome(subtype, chromosome_number, arriba_file,fusioncatcher_file)
+cat(result, "\n")
+
+
+# Create the output text
+output_text <- paste("Based on the gene expression profile, the sample", sample_info,
+                     "was allocated to the", subtype, "subtype with", confidence ,".\n" ,result)
+
+
+# Write the output text to a CSV file
+output_file <- "out.csv"
+write.csv(data.frame(Output=output_text), file=outfile, row.names=FALSE, quote=FALSE)
+
+# Print a message to indicate that the process is complete
+cat("Output file 'out.csv' has been generated.\n")

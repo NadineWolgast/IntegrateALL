ba3f9842246ab8f07599a05d4b85652c2bedebb8
Nadine Wolgast <Nadinekraft@gmx.de>
2024-02-22 09:31:10 +0100

    Update prepare_vcf-files_gatk.R

--- scripts/prepare_vcf-files_gatk.R
+++ scripts/prepare_vcf-files_gatk.R
@@ -1,4 +1,4 @@
-library(conflicted)
+#library(conflicted)
 library(data.table)
 library(readxl)
 library(lubridate)
@@ -18,11 +18,6 @@ file <- read.csv(paste0(vcf_directory,'_sel'),
                  sep = "\t")
 head(file)
 table(file$V1)
-# https://gatk.broadinstitute.org/hc/en-us/articles/360035531692-VCF-Variant-Call-Format
-# AD
-# AD is the unfiltered allele depth, i.e. the number of reads that support each of the reported alleles. All reads at the position (including reads that did not pass the variant caller’s filters) are included in this number, except reads that were considered uninformative. Reads are considered uninformative when they do not provide enough statistical evidence to support one allele over another.
-# AD=18,15 example 18 of which supported the REF allele (=had the reference base) and 15 of which supported the ALT allele (=had the alternate base)
-
 head(file)
 table(file$V9)
 
@@ -30,17 +25,17 @@ table(file$V9)
 AD_pos <- unlist(lapply(sapply(file$V9, function(x) strsplit(x , ":")), function(x) which(x == "AD")))
 
 if (length(which(AD_pos == 2)) == nrow(file)) {
-
+  
   AD <- as.character(sapply(file$V10, function(x) strsplit(x, ":", fixed = T)[[1]][[2]]))
   REF <- as.numeric(as.character(sapply(AD, function(x) strsplit(x, ",", fixed = T)[[1]][[1]])))
   DP <- sapply(AD, function(x) strsplit(x, ",", fixed = T)[[1]])
   DP <- unlist(lapply(DP, function(x) sum(as.numeric(x))))
-
+  
   tab <- data.frame("chr" = paste0("chr", file$V1),
                     "start" = file$V2,
                     "depth" = DP,
                     "maf" = as.numeric((DP-REF)/(DP)),
                     stringsAsFactors = F)
   write.table(tab, output_file, sep = "\t",row.names = F)
-
+  
 }

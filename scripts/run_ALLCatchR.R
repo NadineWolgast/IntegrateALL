9954af9ccdb2e46e616929a5c4c8da880c05f06a
Nadine Wolgast <Nadinekraft@gmx.de>
2024-06-18 11:03:02 +0200

    Add files via upload Updated ALLCatchR version, needed to adjust run_ALCatchR R script

--- scripts/run_ALLCatchR.R
+++ scripts/run_ALLCatchR.R
@@ -1,8 +1,10 @@
-library(ALLCatchR)
+library(ALLCatchRbcrabl1)
 
 # Get input file path from command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 input_file <- args[1]
+out_file <- args[2]
 
-allcatch(input_file, ID_class = "ensemble_ID", sep = "\t")
+#allcatch(input_file, ID_class = "ensemble_ID", sep = "\t")
 
+allcatch_bcrabl1(input_file, ID_class = "ensemble_ID", sep = "\t", out.file = out_file)
